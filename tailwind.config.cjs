1e2db0953acedce58b3661d2a9aca02bb956e6bd
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-10-27 16:52:37 +0530

    tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,17 +3,7 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
-    extend: {
-      colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
-      },
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-      },
-    },
+    extend: {},
     screens: {
       xs: "480px",
       ss: "620px",
